9cc7465c35baf31e032834046f0322249e47c9a6
Madhav Bais <madhvendraxbais@gmail.com>
2024-10-07 23:39:04 +0530

    finishing up last section

--- controllers/campground.js
+++ controllers/campground.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const {cloudinary} = require("../cloudinary")
 
 
 module.exports.index = async (req, res) => {
@@ -49,6 +50,7 @@ module.exports.editCampground = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     // const campground = await Campground.findById(id);
     // if(!campground.author.equals(req.user._id)){
     //     req.flash('error', 'You dont have the permission to do that!')
@@ -58,6 +60,13 @@ module.exports.updateCampground = async (req, res) => {
     const imgs = req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.images.push(...imgs)
     await campground.save()
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+           await cloudinary.uploader.destroy(filename)
+        }
+       await campground.updateOne({$pull: {images: { filename: { $in: req.body.deleteImages}}}})
+       
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,18 @@ const { campgroundSchema } = require('../schemas');
 const Review = require('./reviews')
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
